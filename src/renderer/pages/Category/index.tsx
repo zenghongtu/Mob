a97f71cb60ffa1402ed09f14e2b8329a0aed2fff
zenghongtu <zenghongtu@gmail.com>
2019-04-24 19:30:51 +0800

    feat(renderer): update Category

--- src/renderer/pages/Category/index.tsx
+++ src/renderer/pages/Category/index.tsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import categoryApi, {
+  SubCategoriesItem,
+  AllCategoryInfoItem,
+} from '@/services/category';
 
 import styles from './index.css';
 
+interface CategoryInfoSubItemProps {
+  data: SubCategoriesItem;
+}
+interface CategoryInfoItemProps {
+  data: AllCategoryInfoItem;
+}
+
+const CategoryInfoSubItem = ({ data }: CategoryInfoSubItemProps) => {
+  return (
+    <div>
+      <h5>{data.displayValue}</h5>
+    </div>
+  );
+};
+
+const CategoryInfoSub = ({ data }) => {
+  return (
+    <div>
+      <h3>{data.displayName}</h3>
+      {data.subcategories.map((subItem) => {
+        return <CategoryInfoSubItem data={subItem} />;
+      })}
+    </div>
+  );
+};
+
+const CategoryInfoItem = ({ data }: CategoryInfoItemProps) => {
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      {data.categories.map((item) => {
+        return <CategoryInfoSub data={item} />;
+      })}
+    </div>
+  );
+};
+
+const CategoryInfo = ({ data }) => {
+  return (
+    <div>
+      {data.map((item) => {
+        return <CategoryInfoItem data={item} />;
+      })}
+    </div>
+  );
+};
+
 export default function() {
+  const [allCategoryInfo, setAllCategoryInfo] = useState(null);
+  useEffect(() => {
+    (async () => {
+      const { data } = await categoryApi.getAllCategoryInfo();
+      setAllCategoryInfo(data);
+    })();
+  }, []);
   return (
     <div className={styles.normal}>
-      <h1>Page index</h1>
+      {allCategoryInfo ? <CategoryInfo data={allCategoryInfo} /> : 'loading...'}
     </div>
   );
 }
---
+++ src/renderer/services/category.ts
@@ -0,0 +1,45 @@
+import request from '../utils/request';
+
+export interface SubCategoriesItem {
+  categoryId: number;
+  code: string;
+  displayStatus: number;
+  displayValue: string;
+  id: number;
+  isKeyword: boolean;
+  link: string;
+  metadataId: number;
+  metadataValue: string;
+  metas: any[];
+  position: number;
+}
+
+export interface CategoriesItem {
+  categoryType: number;
+  displayName: string;
+  displayStatus: number;
+  id: number;
+  link: string;
+  name: string;
+  picPath: string;
+  pinyin: string;
+  position: number;
+  subcategories: SubCategoriesItem[];
+}
+
+export interface AllCategoryInfoItem {
+  categories: CategoriesItem[];
+  displayStatus: number;
+  groupType: number;
+  id: number;
+  name: string;
+  position: number;
+}
+
+export type AllCategoryInfo = AllCategoryInfoItem[];
+
+export default {
+  getAllCategoryInfo() {
+    return request.get('/category/allCategoryInfo');
+  },
+};
