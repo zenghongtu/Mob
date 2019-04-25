dc7b760d393c547b6893e946aeed957849528f66
zenghongtu <zenghongtu@gmail.com>
2019-04-25 17:09:40 +0800

    feat(renderer): add Loading
    	new file:   ../components/Loading/index.css
    	new file:   ../components/Loading/index.tsx

---
+++ src/renderer/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import styles from './index.css';
+import { Spin, Icon } from 'antd';
+
+// todo select a beautiful icon
+const antIcon = <Icon type='loading' style={{ fontSize: 30 }} spin />;
+
+export default function() {
+  return (
+    <div className={styles.loadingWrap}>
+      <Spin indicator={antIcon} className={styles.loadingIcon} />
+    </div>
+  );
+}
