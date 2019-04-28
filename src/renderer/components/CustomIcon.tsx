72c10081cb75180637568a0cf0df54744e8c8756
zenghongtu <zenghongtu@gmail.com>
2019-04-28 20:43:49 +0800

    feat(renderer): update CustomIcon script url

--- src/renderer/components/CustomIcon.tsx
+++ src/renderer/components/CustomIcon.tsx
@@ -1,7 +1,7 @@
 import { Icon } from 'antd';
 import { IconProps } from 'antd/lib/icon';
 
-const scriptUrl = '//at.alicdn.com/t/font_1164844_nsq0spdf6w.js';
+const scriptUrl = '//at.alicdn.com/t/font_1164844_unnzoutnpgm.js';
 
 export const CustomIcon: React.SFC<IconProps> = Icon.createFromIconfontCN({
   scriptUrl,
